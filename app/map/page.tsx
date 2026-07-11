8d3d119aa1210c73c09876f433ddfea39be0e91f
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 17:47:49 -0400

    Replace custom SVG route guide with native map route line

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,10 +2,9 @@ import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-workflow-controller.css";
 import "./map-hybrid-b.css";
-import "./map-suggested-route.css";
 import MapClient from "./MapClient";
-import MapGoogleStyleRouteOverlayClient from "./MapGoogleStyleRouteOverlayClient";
 import MapHybridViewModeClient from "./MapHybridViewModeClient";
+import MapNativeRouteLineAutoClient from "./MapNativeRouteLineAutoClient";
 import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
 
 export default function MapPage() {
@@ -14,7 +13,7 @@ export default function MapPage() {
       <MapClient />
       <MapWorkflowControllerClient />
       <MapHybridViewModeClient />
-      <MapGoogleStyleRouteOverlayClient />
+      <MapNativeRouteLineAutoClient />
     </>
   );
 }
