ddc1c5e4afc4e9e836ea2f41ee2b2d004008f87a
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 18:52:10 -0400

    Replace straight guide with road-routed map geometry

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,10 +2,10 @@ import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-workflow-controller.css";
 import "./map-hybrid-b.css";
-import "./map-visible-route-guide.css";
+import "./map-road-route.css";
 import MapClient from "./MapClient";
 import MapHybridViewModeClient from "./MapHybridViewModeClient";
-import MapVisibleRouteGuideClient from "./MapVisibleRouteGuideClient";
+import MapRoadRouteOverlayClient from "./MapRoadRouteOverlayClient";
 import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
 
 export default function MapPage() {
@@ -14,7 +14,7 @@ export default function MapPage() {
       <MapClient />
       <MapWorkflowControllerClient />
       <MapHybridViewModeClient />
-      <MapVisibleRouteGuideClient />
+      <MapRoadRouteOverlayClient />
     </>
   );
 }
