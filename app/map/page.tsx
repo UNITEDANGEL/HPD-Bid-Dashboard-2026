e9477fdde98f840be68d62c090fd2a4cf759d337
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 18:00:18 -0400

    Mount visible route guide instead of hidden native automation

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,9 +2,10 @@ import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-workflow-controller.css";
 import "./map-hybrid-b.css";
+import "./map-visible-route-guide.css";
 import MapClient from "./MapClient";
 import MapHybridViewModeClient from "./MapHybridViewModeClient";
-import MapNativeRouteLineAutoClient from "./MapNativeRouteLineAutoClient";
+import MapVisibleRouteGuideClient from "./MapVisibleRouteGuideClient";
 import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
 
 export default function MapPage() {
@@ -13,7 +14,7 @@ export default function MapPage() {
       <MapClient />
       <MapWorkflowControllerClient />
       <MapHybridViewModeClient />
-      <MapNativeRouteLineAutoClient />
+      <MapVisibleRouteGuideClient />
     </>
   );
 }
