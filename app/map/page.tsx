b57b4ddf0bc40985da12c7112c5e7faea82f865e
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 14:41:38 -0400

    Mount Enroute directions controls on map AI lists

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,14 +1,17 @@
 import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-ai-operations.css";
+import "./map-directions-enhancer.css";
 import MapAiOperationsDashboard from "./MapAiOperationsDashboard";
 import MapClient from "./MapClient";
+import MapDirectionsEnhancer from "./MapDirectionsEnhancer";
 
 export default function MapPage() {
   return (
     <>
       <MapClient />
       <MapAiOperationsDashboard />
+      <MapDirectionsEnhancer />
     </>
   );
 }
