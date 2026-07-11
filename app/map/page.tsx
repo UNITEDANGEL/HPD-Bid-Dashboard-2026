11708fb71f49b6997adc0c2ff191cde82e74982b
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 15:01:10 -0400

    Mount smart dispatcher ETA and summary flow

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -3,10 +3,12 @@ import "./v6-map-stability.css";
 import "./map-ai-operations.css";
 import "./map-directions-enhancer.css";
 import "./map-field-visit-flow.css";
+import "./map-smart-dispatch.css";
 import MapAiOperationsDashboard from "./MapAiOperationsDashboard";
 import MapClient from "./MapClient";
 import MapDirectionsEnhancer from "./MapDirectionsEnhancer";
 import MapFieldVisitFlow from "./MapFieldVisitFlow";
+import MapSmartDispatchEnhancer from "./MapSmartDispatchEnhancer";
 
 export default function MapPage() {
   return (
@@ -15,6 +17,7 @@ export default function MapPage() {
       <MapAiOperationsDashboard />
       <MapDirectionsEnhancer />
       <MapFieldVisitFlow />
+      <MapSmartDispatchEnhancer />
     </>
   );
 }
