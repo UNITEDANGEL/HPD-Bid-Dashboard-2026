748431f5429d7eef1925fd3fe729d122afc8cee4
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 19:13:01 -0400

    Remove inaccurate projected route overlay

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,10 +2,8 @@ import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-workflow-controller.css";
 import "./map-hybrid-b.css";
-import "./map-road-route.css";
 import MapClient from "./MapClient";
 import MapHybridViewModeClient from "./MapHybridViewModeClient";
-import MapRoadRouteOverlayClient from "./MapRoadRouteOverlayClient";
 import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
 
 export default function MapPage() {
@@ -14,7 +12,6 @@ export default function MapPage() {
       <MapClient />
       <MapWorkflowControllerClient />
       <MapHybridViewModeClient />
-      <MapRoadRouteOverlayClient />
     </>
   );
 }
