7cd4bff7d7d490e6302dd4403c3eae70104697d4
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 16:37:39 -0400

    Mount Jobber Onfleet hybrid map-always workspace

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,9 @@
 import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-workflow-controller.css";
+import "./map-hybrid-b.css";
 import MapClient from "./MapClient";
+import MapHybridViewModeClient from "./MapHybridViewModeClient";
 import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
 
 export default function MapPage() {
@@ -9,6 +11,7 @@ export default function MapPage() {
     <>
       <MapClient />
       <MapWorkflowControllerClient />
+      <MapHybridViewModeClient />
     </>
   );
 }
