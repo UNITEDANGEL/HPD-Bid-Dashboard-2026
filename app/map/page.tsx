ff2993c53ba593bc7c77a348e55c0b94fc082c06
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 13:14:51 -0400

    Load polished AI map dashboard layout

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,6 +1,7 @@
 import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-ai-dashboard.css";
+import "./map-ai-dashboard-v2.css";
 import MapAiDashboard from "./MapAiDashboard";
 import MapClient from "./MapClient";
 
