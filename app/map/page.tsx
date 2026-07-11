3240f8d65cf17db49b207988297ba70316d437c2
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 11:39:19 -0400

    Mount AI dashboard inside map page

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,14 @@
 import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
+import "./map-ai-dashboard.css";
+import MapAiDashboard from "./MapAiDashboard";
 import MapClient from "./MapClient";
 
 export default function MapPage() {
-  return <MapClient />;
+  return (
+    <>
+      <MapClient />
+      <MapAiDashboard />
+    </>
+  );
 }
