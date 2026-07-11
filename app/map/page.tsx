b8c75ed16d5d59d76e85e6fb79da85a81f8a5a4a
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 16:44:55 -0400

    Mount suggested route line from current location through stops

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,8 +2,10 @@ import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-workflow-controller.css";
 import "./map-hybrid-b.css";
+import "./map-suggested-route.css";
 import MapClient from "./MapClient";
 import MapHybridViewModeClient from "./MapHybridViewModeClient";
+import MapSuggestedRouteOverlayClient from "./MapSuggestedRouteOverlayClient";
 import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
 
 export default function MapPage() {
@@ -12,6 +14,7 @@ export default function MapPage() {
       <MapClient />
       <MapWorkflowControllerClient />
       <MapHybridViewModeClient />
+      <MapSuggestedRouteOverlayClient />
     </>
   );
 }
