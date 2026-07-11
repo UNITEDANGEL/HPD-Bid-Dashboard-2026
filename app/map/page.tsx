044d4aa5957994b3dc08820ae47c0c1bce142fa4
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-11 16:07:01 -0400

    Replace layered map enhancers with unified workflow controller

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,32 +1,14 @@
 import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
-import "./map-ai-operations.css";
-import "./map-directions-enhancer.css";
-import "./map-field-visit-flow.css";
-import "./map-smart-dispatch.css";
-import "./map-agent-lifecycle.css";
-import "./map-responsive-workspace.css";
-import "./map-flow-v2.css";
-import MapAgentLifecycleClient from "./MapAgentLifecycleClient";
-import MapAiOperationsDashboard from "./MapAiOperationsDashboard";
+import "./map-workflow-controller.css";
 import MapClient from "./MapClient";
-import MapDirectionsEnhancer from "./MapDirectionsEnhancer";
-import MapFieldVisitFlow from "./MapFieldVisitFlow";
-import MapFlowDirectorClient from "./MapFlowDirectorClient";
-import MapResponsiveWorkspaceClient from "./MapResponsiveWorkspaceClient";
-import MapSmartDispatchEnhancer from "./MapSmartDispatchEnhancer";
+import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
 
 export default function MapPage() {
   return (
     <>
       <MapClient />
-      <MapAiOperationsDashboard />
-      <MapDirectionsEnhancer />
-      <MapFieldVisitFlow />
-      <MapSmartDispatchEnhancer />
-      <MapResponsiveWorkspaceClient />
-      <MapAgentLifecycleClient />
-      <MapFlowDirectorClient />
+      <MapWorkflowControllerClient />
     </>
   );
 }
