3a6f6a776663b1987a27508688adb9d8eff964de
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-12 09:29:46 -0400

    Remove confusing active-leg route overlay

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,7 +2,6 @@ import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-workflow-controller.css";
 import "./map-hybrid-b.css";
-import MapActiveLegGuide from "./MapActiveLegGuide";
 import MapClient from "./MapClient";
 import MapHybridViewModeClient from "./MapHybridViewModeClient";
 import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
@@ -13,7 +12,6 @@ export default function MapPage() {
       <MapClient />
       <MapWorkflowControllerClient />
       <MapHybridViewModeClient />
-      <MapActiveLegGuide />
     </>
   );
 }
