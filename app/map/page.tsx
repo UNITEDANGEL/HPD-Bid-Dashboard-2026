2bcf99ac74f36363a438f0c43d9b76267a99494e
UNITEDANGEL <uaconstruction2005@gmail.com>
2026-07-12 09:16:19 -0400

    Mount simple active-leg route guide

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,6 +2,7 @@ import "./v6-map-upgrade.css";
 import "./v6-map-stability.css";
 import "./map-workflow-controller.css";
 import "./map-hybrid-b.css";
+import MapActiveLegGuide from "./MapActiveLegGuide";
 import MapClient from "./MapClient";
 import MapHybridViewModeClient from "./MapHybridViewModeClient";
 import MapWorkflowControllerClient from "./MapWorkflowControllerClient";
@@ -12,6 +13,7 @@ export default function MapPage() {
       <MapClient />
       <MapWorkflowControllerClient />
       <MapHybridViewModeClient />
+      <MapActiveLegGuide />
     </>
   );
 }
